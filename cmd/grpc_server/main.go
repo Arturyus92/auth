0f0110600ff13d65171ea5a491a0c00564672bcb
Artur <61751393+Arturyus92@users.noreply.github.com>
2024-02-11 10:28:05 +0300

    second commit

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/Arturyus92/auth"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      gofakeit.Name(),
+		Email:     gofakeit.Email(),
+		Role:      desc.Role_USER(),
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("User with id: %d created", req.CreateId())
+
+	return &desc.CreateResponse{
+		Id: req.GetId(),
+	}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*desc.UpdateResponse, error) {
+	log.Printf("User with id: %d updated", req.UpdateId())
+
+	return &desc.UpdateResponse{
+		Id: req.UpdateId(),
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*desc.DeleteResponse, error) {
+	log.Printf("User with id: %d deleted", req.DeleteId())
+
+	return &desc.DeleteResponse{
+		Id: req.DeleteId(),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
